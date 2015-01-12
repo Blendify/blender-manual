9e6dc507ab3b6ec0e170138a8a088bf8ec7de611
pepribal <pepribal@c4de1f47-6596-e411-a384-0024e86c2797>
2015-01-12 11:34:47 +0000

    Added functionality to rst_check.py: accept arguments, check broken urls, check unused/missing images

--- tools/rst_check.py
+++ tools/rst_check.py
@@ -1,10 +1,14 @@
 #!/usr/bin/env python3
 
 import os
+import requests, re, sys
+
 # if you want to operate on a subdir, eg: "render"
 SUBDIR = ""
 CURRENT_DIR = os.path.abspath(os.path.dirname(__file__))
 RST_DIR = os.path.normpath(os.path.join(CURRENT_DIR, "..", "manual", SUBDIR))
+
+img_refs = []  # useful for image warnings, it holds the name of all referenced images
 
 
 def rst_files(path):
@@ -29,6 +33,9 @@ def main():
 
         with open(fn, "w", encoding="utf-8") as f:
             data_src = f.write(data_dst)
+    
+    if operation == warn_images:
+        warn_images_output()
 
 
 def warn_long_lines(fn, data_src):
@@ -59,8 +66,81 @@ def warn_long_lines(fn, data_src):
     return None
 
 
+def warn_broken_urls(fn, data_src):
+    """
+    Complain about broken URLs
+    """
+    lines = data_src.split("\n")
+    okcodes = [200, 301, 302]  # OK and redirects
+
+    for i, l in enumerate(lines):
+        match = re.search(r"https?\://[A-Za-z0-9-\._~\:/\?#\[\]@!\$\&'\(\)\*\+,;=%]*",l)
+        if match:
+            url = match.string[match.start():match.end()]
+            try:
+                resp = requests.head(url)
+                if resp.status_code not in okcodes:
+                    print("%s:%d: broken url '%s' (code %d)" % (fn, i + 1, url, resp.status_code))
+                else: print("'%s' OK" % url)  # COMMENT OUT THIS LINE TO SEE BROKEN LINKS ONLY!!
+            except KeyboardInterrupt:
+                raise
+            except:
+                print("%s:%d: broken url '%s' (undefined error)" % (fn, i + 1, url))
+    
+    return None
+
+
+def warn_images(fn, data_src):
+    """
+    Complain about unused and missing images
+    """
+    lines = data_src.split("\n")
+
+    for i, l in enumerate(lines):
+        match = re.search(r"\.\. +figure\:\: +/images/(.*\.(png|gif|jpg))",l)
+        if match:
+            img_refs.append(match.string[match.start(1) : match.end(1)])
+    
+    return None
+
+
+def warn_images_output():
+    """
+    Outputs the results of unused/missing images
+    """
+    imgpath = os.path.normpath(os.path.join(os.path.abspath(os.path.dirname(__file__)), "..", "manual", "images"))
+    img_files_set = set([f for f in os.listdir(imgpath)])
+    img_refs_set = set(img_refs)
+
+    print("\nLIST OF UNUSED IMAGES:")
+    print("======================")
+    l1 = [fn for fn in img_files_set - img_refs_set]
+    l1.sort()
+    for fn in l1:
+        print(fn)
+        
+    print("\nLIST OF MISSING IMAGES:")
+    print("=======================")
+    l2 = [fn for fn in img_refs_set - img_files_set]
+    l2.sort()
+    for fn in l2:
+        print(fn)
+
+
 # define the operation to call
-operation = warn_long_lines
+if len(sys.argv) != 2:
+    operation = None
+elif sys.argv[1] == "--long":
+    operation = warn_long_lines
+elif sys.argv[1] == "--url":
+    operation = warn_broken_urls
+elif sys.argv[1] == "--image":
+    operation = warn_images
+else:
+    operation = None
 
 if __name__ == "__main__":
-    main()
+    if operation:
+        main()
+    else:
+        print("Blender manual checks\n    Usage: rst_check.py { --long | --url | --image }\n")
