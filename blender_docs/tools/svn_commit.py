20c877764cc7dc52720f6c4734fdd852bcbe22c3
campbellbarton <campbellbarton@c4de1f47-6596-e411-a384-0024e86c2797>
2016-01-11 11:28:51 +0000

    Cleanup: pep8

--- blender_docs/tools/svn_commit.py
+++ blender_docs/tools/svn_commit.py
@@ -15,19 +15,22 @@ import os
 
 from file_translation_progress import parse_file
 
+
 def added_files(path):
     for file_path in svn_status(path):
         yield file_path[8:].rstrip()
 
+
 def difference(file_path):
     added = -1
-    deleted = -1    
+    deleted = -1
     for line in svn_diff(file_path):
         if line.startswith('+'):
             added += 1
-        if line.startswith('-') and not '-msgstr ""' in line:
+        if line.startswith('-') and ('-msgstr ""' not in line):
             deleted += 1
     return added, deleted
+
 
 def file_message(file_path):
     import re
@@ -35,23 +38,28 @@ def file_message(file_path):
     report = parse_file(file_path)
     path = re.search('(.*)LC_MESSAGES/', file_path).regs
     file_path = file_path[path[0][1]:]
-    if diff[0] > diff[1]: # something was appended
+    if diff[0] > diff[1]:  # something was appended
         done = report[1] / report[0] * 100
         return 'Translated %.0f%% of %s' % (done, file_path)
     size = 'Minor' if diff[0] < 20 else 'Major'
     return '%s changes in %s' % (size, file_path)
 
+
 def svn_status(path):
     return exec_command('svn status ' + path)
+
 
 def svn_diff(file_path):
     return exec_command('svn diff ' + file_path)
 
+
 def svn_commit(path, message):
     return exec_command('svn commit %s -m "%s"' % (path, message))
 
+
 def exec_command(name):
     return os.popen(name).readlines()
+
 
 def main():
     import argparse
