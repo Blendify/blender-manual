84e51fdb42330ec28d85ff44643d1268052ad2ef
Blendify <Blendify@c4de1f47-6596-e411-a384-0024e86c2797>
2016-04-28 02:17:41 +0000

    [WIP] Add sanity check to windows make file
    Also correct indent? <-- Not sure on this

--- trunk/blender_docs/make.bat
+++ trunk/blender_docs/make.bat
@@ -2,6 +2,13 @@
 
 setlocal ENABLEEXTENSIONS
 
+where /Q python
+if %ERRORLEVEL% NEQ 0 (
+	echo Error: "Python" command not in the PATH.
+	echo You must have Python installed and added to your PATH, aborting!
+	goto EOF
+)
+
 :argv_loop
 if NOT "%1" == "" (
 
@@ -31,6 +38,7 @@ if NOT "%1" == "" (
 		sphinx-build -b html .\manual .\build\html
 	) else if "%1" == "clean" (
 		RMDIR/s/q build
+		sphinx-build -b html .\manual .\build\html
 	)
 	goto EOF
 
@@ -39,8 +47,8 @@ if NOT "%1" == "" (
 		python tools/rst_check_syntax.py --long > rst_check_syntax.log
 		type rst_check_syntax.log
 		DEL rst_check_syntax.log
-	)
-	goto EOF
+		)
+		goto EOF
 	) else if "%1" == "check_structure" (
 		python tools/rst_check_structure.py --image > rst_check_structure.log
 		type rst_check_structure.log
@@ -57,7 +65,7 @@ if NOT "%1" == "" (
 	) else (
 		echo Command "%1" unknown, aborting!
 		goto EOF
-	)
+		)
 
 	shift /1
 	goto argv_loop
