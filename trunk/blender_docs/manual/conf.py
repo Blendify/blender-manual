d061c7cf5ae6b91eb8f30520532c4f9fcce65eff
Blendify <Blendify@c4de1f47-6596-e411-a384-0024e86c2797>
2016-06-10 17:12:42 +0000

    Small fix

--- trunk/blender_docs/manual/conf.py
+++ trunk/blender_docs/manual/conf.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 #
-# Project Name documentation build configuration file, created by
+# Blender Manual documentation build configuration file, created by
 # sphinx-quickstart on Fri Jun 10 12:33:27 2016.
 #
 # This file is execfile()d with the current directory set to its
