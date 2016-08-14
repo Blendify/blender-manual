06bbb428cb2707a6b8266c39d2d73178df4c8e08
Blendify <Blendify@c4de1f47-6596-e411-a384-0024e86c2797>
2016-08-14 22:32:16 +0000

    Include mathjax path

--- blender_docs/manual/conf.py
+++ blender_docs/manual/conf.py
@@ -16,6 +16,8 @@
 import sys
 import os
 
+mathjax_path="https://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML"
+
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
