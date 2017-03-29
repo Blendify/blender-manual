f221332ec321a87fafe87ea09638b94fd0c1cab7
Blendify <Blendify@c4de1f47-6596-e411-a384-0024e86c2797>
2017-03-29 18:19:08 +0000

    Use svg logo instead of png

--- blender_docs/manual/conf.py
+++ blender_docs/manual/conf.py
@@ -190,7 +190,7 @@ html_title = "Blender Manual"
 # of the sidebar.
 #
 # Socket logo from: https://www.blender.org/about/logo
-html_logo = "../resources/theme/sidebar_logo.png"
+html_logo = "../resources/theme/blender-logo.svg"
 
 # The name of an image file (relative to this directory) to use as a favicon of
 # the docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
