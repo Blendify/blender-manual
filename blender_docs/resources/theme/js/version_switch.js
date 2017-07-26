34d030957c6ddb00cdf1b40f17cbdbed180c0c50
campbellbarton <campbellbarton@c4de1f47-6596-e411-a384-0024e86c2797>
2017-07-26 15:47:34 +0000

    Add uk to sidebar

--- blender_docs/resources/theme/js/version_switch.js
+++ blender_docs/resources/theme/js/version_switch.js
@@ -16,6 +16,7 @@ var all_langs = {
 	"ja": "&#x65E5;&#x672C;&#x8A9E;",
 	"pt": "Portugu&ecirc;s",
 	"ru": "&#x3A1;&#x443;&#x441;&#x441;&#x43A;&#x438;&#x439;",
+	"uk": "Ukra&#1111;na",
 	"zh.cn": "&#x4E2D;&#x6587;(&#x7B80;&#x4F53;)",
 	"zh.tw": "&#x4E2D;&#x6587;(&#x7E41;&#x9AD4;)"
 };
