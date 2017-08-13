aa10abb547ee5dcd6426fc6ce7e190a4511490d7
Blendify <Blendify@c4de1f47-6596-e411-a384-0024e86c2797>
2017-08-13 03:28:09 +0000

    Correct Language Name

--- blender_docs/resources/theme/js/version_switch.js
+++ blender_docs/resources/theme/js/version_switch.js
@@ -17,7 +17,7 @@ var all_langs = {
 	"nb": "Norsk Bokm&#x00E5;l",
 	"pt": "Portugu&ecirc;s",
 	"ru": "&#x3A1;&#x443;&#x441;&#x441;&#x43A;&#x438;&#x439;",
-	"fi": "Suomalainen",
+	"fi": "Suomeksi",
 	"uk": "Ukra&#1111;na",
 	"zh-hans": "&#x4E2D;&#x6587;(&#x7B80;&#x4F53;)",
 	"zh-hant": "&#x4E2D;&#x6587;(&#x7E41;&#x9AD4;)"
