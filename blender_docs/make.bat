8cb060a0b43d43b755e9e17713410c340c10bcdb
campbellbarton <campbellbarton@c4de1f47-6596-e411-a384-0024e86c2797>
2018-02-08 05:14:40 +0000

    Add tools_report/ directory

--- blender_docs/make.bat
+++ blender_docs/make.bat
@@ -224,7 +224,7 @@ if "%1" == "update_po" (
 
 if "%1" == "report_po_progress" (
 	IF NOT EXIST %cd%/locale GOTO MISSING_LOCALE
-	python tools/report_translation_progress.py locale/%2 --quiet > po_progress.log
+	python tools_report/report_translation_progress.py locale/%2 --quiet > po_progress.log
 	type po_progress.log
 	DEL po_progress.log
 	goto EOF
