a4bcdad268f6370b48e4e4e34da80363d56ade7b
Blendify <Blendify@c4de1f47-6596-e411-a384-0024e86c2797>
2018-02-24 00:40:27 +0000

    Use default code styling

--- trunk/blender_docs/manual/conf.py
+++ trunk/blender_docs/manual/conf.py
@@ -141,7 +141,7 @@ exclude_patterns += ['_build']
 #show_authors = False
 
 # The name of the Pygments (syntax highlighting) style to use.
-pygments_style = 'sphinx'
+pygments_style = 'default'
 
 # By default, highlight as Python 3.
 highlight_language = 'python3'
