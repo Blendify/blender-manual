779786d2d6a2264e85066ccb20216fcbd1f9a09b
Blendify <Blendify@c4de1f47-6596-e411-a384-0024e86c2797>
2018-10-29 00:49:32 +0000

    Versioning: Add version compare warning for old manuals

--- blender_docs/resources/theme/js/version_switch.js
+++ blender_docs/resources/theme/js/version_switch.js
@@ -72,7 +72,9 @@ afterload: function() {
 	//release = 2.79; //@
 	var lang = DOCUMENTATION_OPTIONS.LANGUAGE;
 	if(lang === "None" || lang === undefined) {lang = "en";}
-
+	
+	this.warn_old(release, all_versions.latest);
+	
 	var version = this.get_named(release);
 	if(this.type) {this.label = all_versions[version];}else{this.label = all_langs[lang];}
 	var list = this.build_list(version, lang);
@@ -86,6 +88,28 @@ afterload: function() {
 	this.btnhandler();
 	this.$btn.on("mousedown", function(e){that.btnhandler(); e.preventDefault()});
 	this.$btn.on("keypress", function(e){ if(that.keybtnfilter(e)){that.btnhandler();} });
+},
+warn_old: function(release, latest) {
+  if (release < latest) {
+    var currentURL = window.location.pathname.replace(release, latest);
+    var warning = $(
+        '<div class="admonition warning"> ' +
+        '<p class="first admonition-title">Note</p> ' +
+        '<p class="last"> ' +
+        'You are not using the most up to date version of the documentation. ' +
+        '<a href="#"></a> is the newest version.' +
+        '</p>' +
+        '</div>');
+
+    warning
+      .find('a')
+      .attr('href', currentURL)
+      .text(latest);
+
+    var body = $("div.body");
+    if (!body.length) {body = $("div.document");}
+    body.prepend(warning);
+	}
 },
 build_list: function(v, l) {
 	var neutral_url = this.get_neutral();
