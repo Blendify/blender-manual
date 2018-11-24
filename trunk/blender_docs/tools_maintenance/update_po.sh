58894441c27440e0aabc822266486520e4977636
hoangduytran <hoangduytran@c4de1f47-6596-e411-a384-0024e86c2797>
2018-11-24 06:41:58 +0000

    20181124 - add changes to make scripts

--- trunk/blender_docs/tools_maintenance/update_po.sh
+++ trunk/blender_docs/tools_maintenance/update_po.sh
@@ -4,7 +4,118 @@
 # This script is to reduce tedious steps involved when updating PO files.
 # It looks more complex then it really is, since we do multi-processing
 # to update the PO files, to save some time.
+# Last-Updater: Hoang Duy Tran <hoangduytran1960@googlemail.com>
+# Last-Update-Date: 2018-11-18 17:51+0000
+#
 
+# Ensure we're in the repo's base:
+function setBaseDirectory()
+{
+	BASEDIR=$(cd "$(dirname 0)"; pwd -P)
+	if [ ! -e Makefile ]; then
+		echo "The base [$BASEDIR] doesn't contain Makefile required. Terminate the process."
+		exit 1
+	else
+		#echo "The base directory is: [$BASEDIR]"
+		cd $BASEDIR
+	fi
+}
+
+# Update the locale dir:
+function updateLocaleDir()
+{
+	#Locate the SVN directory under locale.
+	#It could be deeper than expected, depending on how the source has been checked out
+	LOCALE_SVN_DIR=$(find ./locale -type d -name ".svn" -print)
+	if [ -z $LOCALE_SVN_DIR ]; then
+		echo "Unable to find SVN directory for ./locale"
+		exit 1
+	fi
+	locale_dir=$(dirname $LOCALE_SVN_DIR)
+	cd "$locale_dir"
+	svn cleanup .
+	svn up .
+	cd -
+}
+
+# Create POT files:
+function createPOTFiles()
+{
+	rm -rf build/locale
+	make gettext
+}
+
+# Update PO files
+#
+# note, this can be slow so (multi-process)
+function updatePOFiles()
+{
+	for lang in `find locale/ -maxdepth 1 -mindepth 1 -type d -not -iwholename '*.svn*' -printf '%f\n' | sort`; do
+		sphinx-intl update -p build/locale -l $lang &
+	done
+
+	FAIL=0
+	for job in `jobs -p`; do
+		echo $job
+		wait $job || let "FAIL+=1"
+	done
+	if [ "$FAIL" != "0" ]; then
+		echo "Error updating"
+		exit 1
+	fi
+	unset FAIL
+}
+
+# Add newly created PO files:
+function svnAddPOFiles()
+{
+	cd $LOCALE_SVN_DIR
+	NEW_FILES=`svn status . | grep -e "\.po$" | awk '/^[?]/{print $2}'`
+	if [ "$NEW_FILES" != "" ]; then
+		svn add $NEW_FILES
+	fi
+	unset NEW_FILES
+}
+
+# note, the Python part filters only for directories
+# there may be a cleaner way to do this in shell.
+function svnAddNewDirs()
+{
+	NEW_DIRS=`svn status . | grep -v -e "\.po$" | awk '/^[?]/{print $2}' | python -c "import sys, os; sys.stdout.write('\n'.join([f for f in sys.stdin.read().split('\n') if os.path.isdir(f)]))"`
+	if [ "$NEW_DIRS" != "" ]; then
+		svn add $NEW_DIRS
+	fi
+	unset NEW_DIRS
+	cd -
+}
+
+# Notify on redundant PO files
+function notifyRedundantPOFiles()
+{
+	python3 tools_rst/rst_check_locale.py
+}
+
+# Print Commit message:
+function printCommitMessages()
+{
+	REVISION=`svn info . | grep '^Revision:' | sed -e 's/^Revision: //'`
+	echo " cd locale; svn ci . -m \"Update r"$REVISION\""; cd .."
+}
+
+function performUpdatePO()
+{
+	setBaseDirectory
+	updateLocaleDir
+	createPOTFiles
+	updatePOFiles
+	svnAddPOFiles
+	svnAddNewDirs
+	notifyRedundantPOFiles
+	printCommitMessages
+}
+
+# Python needs utf
+export LANG="en_US.UTF8"
 
 # Trap on the ERR pseudo signal
 # http://stackoverflow.com/a/4384381/432509
@@ -15,70 +126,4 @@ err_trap () {
     exit $errcode
 }
 trap err_trap ERR
-
-# Python needs utf
-export LANG="en_US.UTF8"
-
-# Ensure we're in the repo's base:
-BASEDIR=$(dirname $0)
-cd $BASEDIR
-cd ../
-
-
-# Update the locale dir:
-cd ./locale
-svn cleanup .
-svn up .
-cd ../
-
-
-# Create PO files:
-rm -rf build/locale
-make gettext
-
-
-# Update PO files
-#
-# note, this can be slow so (multi-process)
-for lang in `find locale/ -maxdepth 1 -mindepth 1 -type d -not -iwholename '*.svn*' -printf '%f\n' | sort`; do
-	sphinx-intl update -p build/locale -l $lang &
-done
-
-FAIL=0
-for job in `jobs -p`; do
-echo $job
-    wait $job || let "FAIL+=1"
-done
-if [ "$FAIL" != "0" ]; then
-	echo "Error updating"
-	exit 1
-fi
-unset FAIL
-
-
-# Add newly created PO files:
-cd locale/
-NEW_FILES=`svn status . | grep -e "\.po$" | awk '/^[?]/{print $2}'`
-if [ "$NEW_FILES" != "" ]; then
-	svn add $NEW_FILES
-fi
-unset NEW_FILES
-
-# note, the Python part filters only for directories
-# there may be a cleaner way to do this in shell.
-NEW_DIRS=`svn status . | grep -v -e "\.po$" | awk '/^[?]/{print $2}' | python -c "import sys, os; sys.stdout.write('\n'.join([f for f in sys.stdin.read().split('\n') if os.path.isdir(f)]))"`
-if [ "$NEW_DIRS" != "" ]; then
-	svn add $NEW_DIRS
-fi
-unset NEW_DIRS
-
-cd -
-
-
-# Notify on redundant PO files
-python3 tools_rst/rst_check_locale.py
-
-# Print Commit message:
-REVISION=`svn info . | grep '^Revision:' | sed -e 's/^Revision: //'`
-echo " cd locale; svn ci . -m \"Update r"$REVISION\""; cd .." 
-
+performUpdatePO
--- trunk/blender_docs/tools_rst/rst_check_locale.py
+++ trunk/blender_docs/tools_rst/rst_check_locale.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env python3
 # Apache License, Version 2.0
 # <pep8 compliant>
+# This script is to used to find out PO files that no longer have the RST corresponding instance.
+# and printout the 'svn rm --force <filename>', so they can be captured to a file and run at
+# user's pleasure. This script assumes the existence of 'addons' directory under the locale/ path.
+# Last-Updater: Hoang Duy Tran <hoangduytran1960@googlemail.com>
+# Last-Updated: 2018-11-22 15:12+0000
+
 """
 This tool checks for unused locale files,
 printing out the command to remove them (if any).
@@ -9,6 +15,134 @@ printing out the command to remove them (if any).
 import os
 import sys
 
+# -----------------------------------------------------------------------------
+# ListPathEvent
+# This is the base body of file listing event handler, based on the java's ActionEvent pattern.
+# It only holds mainly the data passing in by the listPath function
+#   1. dirpath : current path of a file
+#   2. dirnames : all the directory names under the dirpath
+#   3. filenames : only the filename part under the dirpath
+# 3 parts must be combined in order to form the whole absolute path to a filename
+#
+class ListPathEvent:
+    def __init__(self):
+        self.dirpath = None
+        self.dirnames = None
+        self.filenames = None
+
+    def __repr__(self):
+        l=[]
+        if (self.dirpath):
+            for dir_path in self.dirpath:
+                l.append(dir_path)
+
+        if (self.dirnames):
+            for dir_name in self.dirnames:
+                l.append(dir_name)
+
+        if (self.filenames):
+            for file_name in self.filenames:
+                l.append(file_name)
+        return "".join(l)
+
+    def setVars(self, dirpath, dirnames, filenames):
+        self.dirpath = dirpath
+        self.dirnames = dirnames
+        self.filenames = filenames
+
+    def run(self):
+        pass
+
+
+# -----------------------------------------------------------------------------
+# findParentDir
+# An instance of implementation of the ListPathEvent
+# Search for a directory name and return the parent directory of search_path
+# Calling it by x = findParentDir(".svn")
+# get result by x.result
+# return the part BEFORE the ".svn", ie. <locale_dir> (ROOT_DIR/locale/fr)
+#
+class findParentDir(ListPathEvent):
+
+    def __init__(self, search_path):
+        self.search_path = search_path
+        self.result = None
+
+    def run(self):
+        valid = ((self.dirnames != None) and (self.search_path in self.dirnames))
+        if (valid):
+            self.result = self.dirpath+os.sep
+            #print("self.result: {}, dirnames: {}".format(self.result, self.dirnames))
+            return
+
+
+# -----------------------------------------------------------------------------
+# findFileByExtension
+# An instance of implementation of the ListPathEvent
+# Find all files with matching extenion
+# Calling it by x = findFileByExtension("rst")
+# return the list of all files matching the provided extension
+#
+class findFileByExtension(ListPathEvent):
+
+    def __init__(self, search_extension):
+        self.search_extension = search_extension
+        self.result = []
+
+    def run(self):
+        for filename in self.filenames:
+            ext = os.path.splitext(filename)[1]
+            is_valid = ext.lower().endswith(self.search_extension)
+            if (is_valid):
+                entry=os.path.join(self.dirpath, filename)
+                self.result.append(entry)
+                #print("entry:{}".format(entry))
+
+# -----------------------------------------------------------------------------
+# findFileByExtensionRelative
+# An instance of implementation of the ListPathEvent
+# Find all files with matching extenion
+# Calling it by x = findFileByExtension("rst")
+# return the list of all files matching the provided extension, with relative paths
+#
+class findFileByExtensionRelative(ListPathEvent):
+
+    def __init__(self, root_dir, search_extension):
+        self.search_extension = search_extension
+        self.root_dir = root_dir
+        self.result = []
+
+    def run(self):
+        excluded_len = len(self.root_dir)
+        for filename in self.filenames:
+            ext = os.path.splitext(filename)[1]
+            valid = ext.lower().endswith(self.search_extension)
+            if (valid):
+                entry=os.path.join(self.dirpath, filename)
+                rel_path = entry[excluded_len:]
+                self.result.append(rel_path)
+
+# -----------------------------------------------------------------------------
+# findFileByName
+# An instance of implementation of the ListPathEvent
+# Find all files with the same name in all subdirectories
+# Calling it by x = findFileByName("index.po")
+# Get result by x.result
+# return the list of all matching the provided name in all subdirectories
+#
+class findFileByName(ListPathEvent):
+
+    def __init__(self, search_name):
+        self.search_name = search_name
+        self.result = []
+
+    def run(self):
+        for filename in self.filenames:
+            is_found = (filename.lower() == self.search_name.lower())
+            if (is_found):
+                entry=os.path.join(self.dirpath, filename)
+                self.result.append(entry)
+
 
 # if you want to operate on a subdir, e.g: "render"
 SUBDIR = ""
@@ -16,58 +150,121 @@ CURRENT_DIR = os.path.abspath(os.path.dirname(__file__))
 ROOT_DIR = os.path.normpath(os.path.join(CURRENT_DIR, ".."))
 RST_DIR = os.path.join(ROOT_DIR, "manual", SUBDIR)
 LOCALE_DIR = os.path.join(ROOT_DIR, "locale")
+SHELL_EXT = (".bat" if (sys.platform == "win32") else ".sh")
+COMMENT_FLAG = ("rem" if (sys.platform == "win32") else "#")
+HOME_DIR=os.path.expanduser("~")
+
+#some default constants
+DOT="."
+PO=".po"
+RST=".rst"
+SVN=".svn"
+REMOVAL_SCRIPT_NAME="remove_po_files{}".format(SHELL_EXT)
+REMOVAL_SCRIPT_PATH=os.path.join(HOME_DIR, REMOVAL_SCRIPT_NAME)
+
+#print("REMOVAL_SCRIPT_PATH {}".format(REMOVAL_SCRIPT_PATH))
+#exit(1)
+#Use this to find the parent path to the directory where '.po' files reside
+#This is potentially made the code goes wrong if the 'addons' directory is changed to something else
+PO_FIND_DIR="addons"
+#print("CURRENT_DIR: {}\nROOT_DIR: {}\nRST_DIR: {}\nLOCALE_DIR: {}".format(CURRENT_DIR, ROOT_DIR, RST_DIR, LOCALE_DIR))
 
 # -----------------------------------------------------------------------------
-# Common Utilities
-
-
-def files_recursive(path, ext_test):
-    for dirpath, dirnames, filenames in os.walk(path):
-        if dirpath.startswith("."):
-            continue
-
-        for filename in filenames:
-            ext = os.path.splitext(filename)[1]
-            if ext.lower().endswith(ext_test):
-                yield os.path.join(dirpath, filename)
-
-
+# print_title
+# Print out a string to the standard output,
+# with uppercase and double underline matching the length of input string
+#
 def print_title(title, underline="="):
     print(f"\n{title.upper()}\n{len(title) * underline}")
 
 
 # -----------------------------------------------------------------------------
-# Locale Checks
+# Common Utilities
+# base function to list path based on the condition and actions defined in the run routine of the callback function
+# remember the callback function must be an instance of ListPathEvent, so your function must inherit that
+def listPath(path, callback):
+    for dirpath, dirnames, filenames in os.walk(path):
+        if dirpath.startswith(DOT):
+            continue
 
+        valid_function = ((not callback is None) and (isinstance(callback, ListPathEvent)))
+        if (valid_function):
+            callback.setVars(dirpath, dirnames, filenames)
+            callback.run()
+
+
+# -----------------------------------------------------------------------------
+# changeExtension
+# swap the existing extension with a new one
+def changeExtension(entry, to_ext):
+    r_index = entry.rfind(DOT)
+    new_entry = entry[:r_index]+to_ext
+    return new_entry
+
+
+def writeFile(file_name, text):
+    try:
+        with open(file_name, "w+") as f:
+            f.write(text)
+            f.close()
+    except Exception as e:
+        print("Error: " + str(e))
+        exit(1)
+
+def writeExecutableShellScript(executable_entry_list, exec_directory):
+    text_for_entries = os.linesep.join(executable_entry_list)
+    cd_exec_dir_text = "cd {}{}".format(exec_directory, os.linesep)
+    cd_return_dir = "cd {}{}".format(ROOT_DIR, os.linesep)
+    writing_text = "{} Run this script to remove spurious PO files that should no longer be there.{}{}{}{}{}".\
+        format(COMMENT_FLAG, os.linesep, cd_exec_dir_text, text_for_entries, os.linesep, cd_return_dir)
+    writeFile(REMOVAL_SCRIPT_PATH, writing_text)
+
+
+# -----------------------------------------------------------------------------
+# Locale Checks
 def warn_locale():
     """
     Check for stale PO files.
     """
-    files_rst = list(files_recursive(RST_DIR, ext_test=".rst"))
-    files_po = list(files_recursive(LOCALE_DIR, ext_test=".po"))
+    find_by_dir=findParentDir(SVN)
+    listPath(LOCALE_DIR, find_by_dir)
+    if (not find_by_dir.result):
+        raise Exception("Unable to find SVN directory.")
 
-    print_title("List of unused locale:")
+    locale_dir = find_by_dir.result
+    find_by_dir = findParentDir(PO_FIND_DIR)
+    listPath(locale_dir, find_by_dir)
+    po_dir = find_by_dir.result
 
-    if files_po:
-        print(" cd locale")
-        for f in files_po:
-            # strip LOCALE_DIR from start
-            f_sub = f[len(LOCALE_DIR) + 1:-2] + "rst"
-            # strip 'fr/LC_MESSAGES'
-            f_sub = os.sep.join(f_sub.split(os.sep)[2:])
-            f_po_as_rst = os.path.join(RST_DIR, f_sub)
-            if not os.path.exists(f_po_as_rst):
-                print(" svn rm --force %s" % f[len(LOCALE_DIR) + 1:])
-        print(" cd ../")
-    return 0
+    len_local_dir = len(locale_dir)
+    po_dir_patch_part = po_dir[len_local_dir:]
 
+
+    find_po_files = findFileByExtensionRelative(po_dir, PO)
+    listPath(LOCALE_DIR, find_po_files)
+    po_file_list = sorted(find_po_files.result)
+
+    unfound_unique_po_list=[]
+    for po_file in po_file_list:
+        rst_file = changeExtension(po_file, ".rst")
+        rst_entry = os.path.join(RST_DIR, rst_file)
+        is_file_existed = os.path.exists(rst_entry)
+        if (not is_file_existed):
+            po_unfound_entry = "svn rm --force {}{}".format(po_dir_patch_part, po_file)
+            if (not po_unfound_entry in unfound_unique_po_list):
+                unfound_unique_po_list.append(po_unfound_entry)
+
+    if (len(unfound_unique_po_list) > 0):
+        print_title("Found SVN entries should no longer exists and can be removed:")
+        writeExecutableShellScript(unfound_unique_po_list, locale_dir)
+        print("Please execute this script: {}".format(REMOVAL_SCRIPT_PATH))
 
 def main():
     if "--help" in sys.argv:
         print(__doc__)
         sys.exit(0)
 
-    return warn_locale()
+    warn_locale()
 
 
 if __name__ == "__main__":
