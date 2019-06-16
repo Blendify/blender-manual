def6e7b57d2669429abb62613666ff54ddf07ea7
campbellbarton <campbellbarton@c4de1f47-6596-e411-a384-0024e86c2797>
2019-06-16 00:20:08 +0000

    Makefile: move opening a quickfix window to a utility, support emacs
    Other editors can be added too.

---
+++ blender_docs/tools/open_quickfix_in_editor.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+
+"""
+Utility to open an warning/error reports in an editor, a list of locations:
+
+/path/file.ext:line:col:
+"""
+
+import sys
+import shutil
+import subprocess
+
+
+def run(cmd):
+    proc = subprocess.Popen(cmd, shell=False)
+    proc.wait()
+    return proc.returncode
+
+
+logfile = sys.argv[-1]
+
+if shutil.which("emacsclient"):
+    sys.exit(run((
+    	"emacsclient",
+        "--eval",
+        f"(progn (find-file \"{logfile}\") (compilation-mode) (end-of-buffer))",
+        "--no-wait",
+		"--alternate-editor=emacs --eval",
+    )))
+
+elif shutil.which("gvim"):
+    sys.exit(run((
+		"gvim",
+        "--nofork",
+        "-c",
+        "cfile rst_check_syntax.log",
+        "-c"
+        "cope",
+        "-c",
+        "clast",
+    )))
+else:
+    print("Error, no editor found (emacsclient, gvim)")
+    sys.exit(1)
