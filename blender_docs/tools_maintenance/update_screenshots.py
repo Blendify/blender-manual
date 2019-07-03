046c9e54e3773159e9e91829aed1b4d483dd3582
campbellbarton <campbellbarton@c4de1f47-6596-e411-a384-0024e86c2797>
2019-07-03 11:52:22 +0000

    update_screenshots: refactor, move logic into functions

--- blender_docs/tools_maintenance/update_screenshots.py
+++ blender_docs/tools_maintenance/update_screenshots.py
@@ -1,16 +1,26 @@
 #!/usr/bin/env python3
 """
+******************
+Update Screenshots
+******************
+
 This utility is intended to update screenshots, currently it covers:
 
 - Preferences (preferences_section_*.png).
 
-Once this command has finshed, review:
+Once this command has finished, review:
 
    manual_images_preview.html
 
 If the images are acceptable, run:
 
    mv manual_images_preview/* manual/images/
+
+
+Example Usage
+=============
+
+   env BLENDER_BIN=/src/blender/blender.bin ./tools_maintenance/update_screenshots.py
 """
 
 import sys
@@ -23,7 +33,9 @@ if "bpy" not in sys.modules:
         blender,
         "--factory-startup",
         "-noaudio",
-        "--no-window-focus",
+        # Note: opening preferences requires window focus.
+        # ideally we could work around this.
+        # "--no-window-focus",
         "--no-native-pixels",
         # For preferences this wont matter, for other screen-shots it may.
         # "--window-geometry", "0", "0", "800", "600",
@@ -36,6 +48,8 @@ if "bpy" not in sys.modules:
     )
     sys.exit(0)
 
+# -----------
+# Setup Paths
 
 ROOT_DIR = os.path.normpath(os.path.join(os.path.dirname(__file__), ".."))
 
@@ -46,6 +60,15 @@ if not os.path.exists(IMAGE_DIR_PREVIEW):
     os.mkdir(IMAGE_DIR_PREVIEW)
 
 
+# -------------
+# Other Globals
+
+# Workaround for previews not being generated instantly.
+# Wait for this many ticks before taking a screenshot.
+SCREENSHOT_WAIT_TICKS = 4
+
+# ----------------------------------------------------------------------
+# Blender Defaults
 
 def setup_default_preferences(preferences):
     """ Set preferences useful for automation.
@@ -60,14 +83,14 @@ def setup_default_preferences(preferences):
 
 
 import bpy
-from bpy import context
 
-setup_default_preferences(context.preferences)
 
-window = context.window
+# ----------------------------------------------------------------------
+# Blender Timer Wrapper
 
-def run(event_iter):
+def run_iter_from_timer(event_iter):
     i = iter(event_iter)
+
     def event_step():
         ret = next(i, Ellipsis)
         if ret is Ellipsis:
@@ -77,24 +100,43 @@ def run(event_iter):
     bpy.app.timers.register(event_step, first_interval=0.0)
 
 
-def tap_key(window, type):
+# ----------------------------------------------------------------------
+# Blender Helpers
+
+def window_tap_key(*, window, type):
     window.event_simulate(type=type, value='PRESS')
     window.event_simulate(type=type, value='RELEASE')
 
 
-def screenshot_preferences():
+def window_screenshot_to_filepath(*, window, filepath):
+    for _ in range(SCREENSHOT_WAIT_TICKS):
+        yield
+
+    bpy.ops.screen.screenshot(
+        {"window": window},
+        filepath=filepath,
+    )
+    yield
+
+
+# ----------------------------------------------------------------------
+# Screenshot Preferences
+
+def screenshot_preferences(window):
+    from bpy import context
+
     prefs = context.preferences
 
     yield
 
+    # We can't open preferences from a timer, use the shortcut.
     # bpy.ops.screen.userpref_show({"window": window, "screen": window.screen}, 'INVOKE_DEFAULT')
-    tap_key(window, 'F4')
+    window_tap_key(window=window, type='F4')
     yield
-    tap_key(window, 'P')
+    window_tap_key(window=window, type='P')
     yield
 
     prefs_window = next(iter([w for w in context.window_manager.windows if w.screen.is_temporary]))
-    print(prefs_window.screen)
     area = prefs_window.screen.areas[0]
 
     prefs_sections = tuple(
@@ -109,12 +151,9 @@ def screenshot_preferences():
         )
         setattr(prefs, "active_section", section)
 
-        yield
-        bpy.ops.screen.screenshot(
-            {"window": prefs_window},
-            filepath=filepath,
-        )
-        yield
+        yield from window_screenshot_to_filepath(window=prefs_window, filepath=filepath)
+
+    bpy.ops.wm.window_close({"window": prefs_window})
 
     yield
 
@@ -122,40 +161,61 @@ def screenshot_preferences():
     # IPython.embed()
 
     bpy.app.use_event_simulate = False
+    prefs.is_dirty = False
 
     yield
 
     print(__doc__)
 
+
     sys.exit(0)
-
-
-run(screenshot_preferences())
 
 
 # ----------------------------------------------------------------------
 # Generate HTML
+#
+# Run this after all other operations.
 
-with open(os.path.join(ROOT_DIR, "manual_images_preview.html"), 'w', encoding='utf-8') as fh:
-    fw = fh.write
-    fw('<!DOCTYPE html>\n')
-    fw('<html>\n')
-    fw('<body>\n')
+def generate_preview_html():
+    with open(os.path.join(ROOT_DIR, "manual_images_preview.html"), 'w', encoding='utf-8') as fh:
+        fw = fh.write
+        fw('<!DOCTYPE html>\n')
+        fw('<html>\n')
+        fw('<body>\n')
 
-    fw('<table border = "1">\n')
+        fw('<table border = "1">\n')
 
-    for filename in os.listdir(IMAGE_DIR_PREVIEW):
+        for filename in os.listdir(IMAGE_DIR_PREVIEW):
 
-        file_old = os.path.relpath(os.path.join(IMAGE_DIR_FINAL, filename), ROOT_DIR)
-        file_new = os.path.relpath(os.path.join(IMAGE_DIR_PREVIEW, filename), ROOT_DIR)
+            file_old = os.path.relpath(os.path.join(IMAGE_DIR_FINAL, filename), ROOT_DIR)
+            file_new = os.path.relpath(os.path.join(IMAGE_DIR_PREVIEW, filename), ROOT_DIR)
 
-        fw('  <tr>\n')
-        fw('    <td><img src="{:s}"></td>\n'.format(file_old))
-        fw('    <td><img src="{:s}"></td>\n'.format(file_new))
-        fw('  </tr>\n')
+            fw('  <tr>\n')
+            fw('    <td><img src="{:s}"></td>\n'.format(file_old))
+            fw('    <td><img src="{:s}"></td>\n'.format(file_new))
+            fw('  </tr>\n')
 
-    fw('</table>\n')
+        fw('</table>\n')
+
+        fw('</body>\n')
+        fw('</html>\n')
 
 
-    fw('</body>\n')
-    fw('</html>\n')
+def screenshot_all(window):
+    yield from screenshot_preferences(window)
+
+    # Finally
+    generate_preview_html()
+    yield
+
+
+def main():
+    from bpy import context
+
+    setup_default_preferences(context.preferences)
+
+    run_iter_from_timer(screenshot_all(context.window))
+
+
+if __name__ == "__main__":
+    main()
