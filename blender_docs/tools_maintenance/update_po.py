86118dafcd072fc6ab007551ccc0a684b3907394
Blendify <Blendify@c4de1f47-6596-e411-a384-0024e86c2797>
2019-08-05 00:13:40 +0000

    Update Po python script more work in progress
    Things that are not working:
    
    - svn_up
    - warn_locale()

--- blender_docs/tools_maintenance/update_po.py
+++ blender_docs/tools_maintenance/update_po.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 # Apache License, Version 2.0
 
 # This script is to reduce tedious steps involved when updating PO files.
@@ -18,8 +19,10 @@
 # This works since subversion will detect the parent directories ".svn"
 # path without us having to change directories.
 
-import os
-from subprocess import Popen, PIPE
+import os, sys, shutil
+import subprocess
+sys.path.append('../tools_rst')
+import rst_check_locale
 
 def find_vcs_root(test, dirs=(".git", ".svn"), default=None):
     prev, test = None, os.path.abspath(test)
@@ -37,6 +40,42 @@ def dir_list_folder(head_dir, dir_name):
                 outputList.append(os.path.join(os.path.join(root, d), os.pardir))
     return outputList
 
+def svn_up(directory):
+    cmd = (
+        "sphinx-intl %s" % directory,
+        "svn up %s" % directory,
+    )
+    p = subprocess.Popen(
+        cmd,
+        stdout=subprocess.PIPE,
+        shell=True,
+    )
+    print(p.stdout.read().strip().decode('utf-8'))
+
+def build_gettext(input, output):
+    import multiprocessing
+    threads = multiprocessing.cpu_count()
+    cmd = (
+        "sphinx-build -j %s -t builder_html -b gettext %s %s" % (threads, input, output)
+    )
+    p = subprocess.Popen(
+        cmd,
+        stdout=subprocess.PIPE,
+        shell=True,
+    )
+    print(p.stdout.read().strip().decode('utf-8'))
+
+def update_po(config, build_dir, lang):
+    cmd = (
+        "sphinx-intl --config=%s update --pot-dir=%s --language=%s" % (config, build_dir, lang)
+    )
+    p = subprocess.Popen(
+        cmd,
+        stdout=subprocess.PIPE,
+        shell=True,
+    )
+    print(p.stdout.read().strip().decode('utf-8'))
+
 CURRENT_DIR = os.path.abspath(os.path.dirname(__file__))
 BASE_DIR = find_vcs_root(CURRENT_DIR)
 LOCALDIR = os.path.abspath(os.path.join(BASE_DIR, 'locale'))
@@ -44,30 +83,36 @@ PO_LANG = [dir for dir in os.listdir(LOCALDIR) if dir not in {'.svn'}]
 SVN_DIRS_ALL = dir_list_folder(LOCALDIR, '.svn')
 
 for SVN_DIR in SVN_DIRS_ALL:
-    os.system('svn cleanup %s' % SVN_DIR)
-    os.system('svn up %s' % SVN_DIR)
+    svn_up(SVN_DIR)
 
-#
-# # Create PO files:
 # make clean
-# make gettext
-#
-#
-# # Update PO files
-# #
-# # note, this can be slow so (multi-process)
+if os.path.exists(os.path.join(BASE_DIR, 'build')):
+    shutil.rmtree(os.path.join(BASE_DIR, 'build'))
+
+build_gettext(
+    os.path.join(BASE_DIR, 'manual'),
+    os.path.join(BASE_DIR, 'build/locale'),
+)
+
 for lang in PO_LANG:
-    #sphinx-intl --config=manual/conf.py update --pot-dir=build/locale --language="$PO_LANG"
+    update_po(
+        os.path.join(BASE_DIR, 'manual/conf.py'),
+        os.path.join(BASE_DIR, 'build/locale'),
+        lang
+    )
 
 for SVN_DIR in SVN_DIRS_ALL:
     os.system('svn --force --depth infinity add %s .' % SVN_DIR)
 
-#
-# # Notify on redundant PO files
-# python3 tools_rst/rst_check_locale.py
-#
-# # Print Commit message:
-# REVISION=$(svn info "$ROOTDIR" | grep '^Revision:' | sed -e 's/^Revision: //')
-# for SVNDIR in "$SVN_DIRS_ALL"; do
-#   echo " svn ci \"$SVNDIR\" -m \"Update r"$REVISION\"""
-# done
+# Notify on redundant PO files
+# not working
+#warn_locale()
+
+def svnversion():
+    p = subprocess.Popen("svnversion", stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    (stdout, stderr) = p.communicate()
+    return stdout
+
+for SVN_DIR in SVN_DIRS_ALL:
+    revision = svnversion()
+    print("svn ci " + SVN_DIR + " -m \"Update Po-files r"  + revision + "\"")
