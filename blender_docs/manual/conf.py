bc11f5c1215b823d251fea9958a04f8a381199b6
Blendify <Blendify@c4de1f47-6596-e411-a384-0024e86c2797>
2019-11-23 00:23:31 +0000

    Bump Manual Version to 2.82

--- blender_docs/manual/conf.py
+++ blender_docs/manual/conf.py
@@ -23,7 +23,7 @@ import os
 # Not used directly by Sphinx, only used by this file.
 #
 
-blender_version = '2.81'
+blender_version = '2.82'
 
 
 # If extensions (or modules to document with autodoc) are in another directory,
