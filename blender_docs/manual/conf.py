d90f0d845c6fc69618c540711406c32d4da8812a
Blendify <Blendify@c4de1f47-6596-e411-a384-0024e86c2797>
2020-01-20 17:55:28 +0000

    Conf: Enable copy source files
    This should not be needed but there is a bug in the theme.

--- blender_docs/manual/conf.py
+++ blender_docs/manual/conf.py
@@ -255,7 +255,9 @@ html_additional_pages = {
 
 # If true, links to the reST sources are added to the pages.
 html_show_sourcelink = False
-html_copy_source = False
+
+# Enable so html search results show context
+html_copy_source = True
 
 # If true, "Created using Sphinx" is shown in the HTML footer. Default is True.
 html_show_sphinx = False
