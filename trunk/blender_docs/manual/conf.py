64cc0389e73f0d9a40307ef70b56acc743af4cd4
Blendify <Blendify@c4de1f47-6596-e411-a384-0024e86c2797>
2020-01-20 19:06:30 +0000

    Revert Last Commit

--- trunk/blender_docs/manual/conf.py
+++ trunk/blender_docs/manual/conf.py
@@ -255,9 +255,7 @@ html_additional_pages = {
 
 # If true, links to the reST sources are added to the pages.
 html_show_sourcelink = False
-
-# Enable so html search results show context
-html_copy_source = True
+html_copy_source = False
 
 # If true, "Created using Sphinx" is shown in the HTML footer. Default is True.
 html_show_sphinx = False
